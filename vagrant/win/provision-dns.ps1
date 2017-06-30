8d63c92702eee35c62e116f119d9f4323dca18c8
David Baumann <daBONDi@users.noreply.github.com>
2017-07-01 01:13:48 +0200

    Fix DNS Provision Script

--- vagrant/win/provision-dns.ps1
+++ vagrant/win/provision-dns.ps1
@@ -1,10 +1,13 @@
 $dns = $($args[0])
+$ip = $($args[1])
 
 Write-Host "Setup DNS: $($dns)"
+Write-Host "Private Network Address: $($ip)"
 
-# TODO: Automatic Detect Internal Adapter, Use Defined IP Address
-$VagrantAdapter = Get-NetAdapter "Ethernet"
-$VMAdapter = Get-NetAdapter "Ethernet 2"
+# Find Private Network
+$VMAdapterIfIndex = (Get-NetIPAddress | Where-Object IPAddress -eq $ip).InterfaceIndex
+$VMAdapter = Get-NetAdapter -InterfaceIndex $VMAdapterIfIndex
+$VagrantAdapter = Get-NetAdapter | Where-Object ifIndex -ne $VMAdapterIfIndex
 
 $VagrantAdapterIfIndex = $VagrantAdapter.ifIndex
 $VMAdapterIfIndex =  $VMAdapter.ifIndex
@@ -12,6 +15,10 @@ $VMAdapterIfIndex =  $VMAdapter.ifIndex
 # Disable DNS Registration on Vagrant Adapter
 Write-Host "Disable DNS Self Registration on Vagrant Adapter"
 Get-DNSClient -InterfaceIndex $VagrantAdapterIfIndex | Set-DNSClient -RegisterThisConnectionsAddress $false
+
+# Setup DNS on Vagrant Adapter
+Write-Host "Setup DNS Server on Vagrant Adapter"
+Set-DnsClientServerAddress -InterfaceIndex $VagrantAdapterIfIndex -ServerAddresses $dns
 
 # Setup DNS on VM Adapter
 Write-Host "Setup DNS Server on VM Adapter"
