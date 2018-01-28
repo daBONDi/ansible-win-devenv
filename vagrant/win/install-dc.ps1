95ed4d7c9de9c3fd7768d9ace3e46541754f6f0f
David Baumann <administrator.bau@josephinum.at>
2018-01-28 01:11:30 +0100

    Finish DC Installation Script

--- vagrant/win/install-dc.ps1
+++ vagrant/win/install-dc.ps1
@@ -1,17 +1,31 @@
+#####################################################################
+#   Install Domain Controller                                       #
+#####################################################################
+
+# Arguments getting passed from Vagrant File
 $ActiveDirectoryFQDN = $args[0]
 $ActiveDirectoryNetbios = $args[1]
 
-Add-WindowsFeature AD-Domain-Services
+# Add needed Windows Features
+Write-Host "Adding AD-Domain-Services Windows Feature"
+Add-WindowsFeature AD-Domain-Services | Out-Null
 
+# Install the Domain Controller
 Import-Module ADDSDeployment
 
-Install-ADDsDomain -CreateDnsDelegation:$false `
- -DatabasePath "C:\Windows\NTDS" `
- -DomainMode "Win2012R2" `
- -DomainName $ActiveDirectoryFQDN `
- -DomainNetbiosName $ActiveDirectoryNetbios `
- -InstallDns:$true `
- -LogPath "C:\Windows\NTDS" `
- -NoRebootOnCompletion:$true `
- -SysvolPath "C:\Windows\SYSVOL" `
- -Force:$true
+$safemodepw = ConvertTo-SecureString "P@ssW0rD!" -AsPlainText -Force
+
+Write-Host "Configuring Active Directory Domain $ActiveDirectoryFQDN($ActiveDirectoryNetbios)"
+Install-ADDSForest -SafeModeAdministratorPassword $safemodepw `
+  -DomainName $ActiveDirectoryFQDN `
+  -DomainNetbiosName $ActiveDirectoryNetbios `
+  -Confirm:$false `
+  -InstallDns `
+  -CreateDnsDelegation:$false `
+  -Force | Out-Null
+Write-Host "Finished Configure Active Directory Domain, need a Restart"
+
+# Need this Workaround see
+# https://github.com/dbroeglin/windows-lab/issues/1
+Write-Host "Start sleeping until reboot to prevent vagrant connection failures..."
+Start-Sleep 180
